2ce487a449d598cce3189e2e52ac2ed45c0efc38
Kozionov <e-kozionov@mail.ru>
2020-12-14 15:34:47 +1200

    first lesson

--- WelcomeToSwift/main.swift
+++ WelcomeToSwift/main.swift
@@ -7,5 +7,54 @@
 
 import Foundation
 
+//var переменная
+//let константа
+
+
+
+let employCountVisits = [2, 3, 5]
+
+//Массив массивов
+let upperEmploy: [[String]] = [
+    ["rrrr", "sfsffd", "sddss"],
+    ["fdfdfd", "sdddd"]
+]
+//Dictionary
+var namesOfIntegers: [String: String] = [:]
+namesOfIntegers["Swift"] = "Язык разработки под iOS"
+namesOfIntegers["ObjectiveC"] = "Язык разработки под iOS, который предшевствовал Swift"
+
+print(namesOfIntegers["Swift"])
+
+//Коллекции
+let employList: [String] = [
+    "Петр Феодосеев",
+    "Геннадий Новиков",
+    "Давид С"
+]
+print(employList)
+
+let employListSet: Set<String> = [
+    "Петр Феодосеев",
+    "Давид С",
+    "Давид С"
+]
+print(employListSet)
+
+var r: Double? = .none
+r = 5
+if let value = r {
+    print(value)
+} else  {
+    print("Сервер не прислал ничего")
+}
+// еще вариант
+let result = r ?? 0
+
+switch r {
+    case .none: print("Server ничего не вернул")
+    case let .some(value): print(value)
+}
+
 print("Hello, World!")
 
